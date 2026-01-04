06212a5710509fe6344649bdcbcb50862f16327d
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2026-01-04 19:40:24 +0100

    rubo

--- db/migrate/20260104114516_move_goodreads_url_to_generic_links.rb
+++ db/migrate/20260104114516_move_goodreads_url_to_generic_links.rb
@@ -11,7 +11,7 @@ class MoveGoodreadsUrlToGenericLinks < ActiveRecord::Migration[8.1]
     BookStub.where.not(goodreads_url: nil).find_each do |book|
       GenericLinkStub.where(entity_type: 'Book', entity_id: book.id, name: 'goodreads').first_or_create!(
         url: book.goodreads_url
-        )
+      )
     end
     remove_column :books, :goodreads_url
     remove_column :books, :goodreads_popularity
