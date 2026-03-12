93820c302f05b227922559fae61308c9e819201c
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2026-03-12 11:16:24 +0100

    chore: security fixes (gh permissions; wiki regex; csrf protection) (#87)
    * fix: restricted regex for wiki locale
    
    * fix: unnecessary csrf skip
    
    * chore: try basic gh permissions
    
    * fix: permissions for pr comments
    
    * statuses

--- app/services/info_fetchers/wiki/url_parser.rb
+++ app/services/info_fetchers/wiki/url_parser.rb
@@ -2,7 +2,7 @@ module InfoFetchers
   module Wiki
     class UrlParser
       def self.extract_base_name_and_locale(url)
-        locale = url.match(%r{^https?://(.*)\.wikipedia\.org/})&.[](1)
+        locale = url.match(%r{^https?://([\w-]*)\.wikipedia\.org/})&.[](1)
         name = url[%r{wiki/(.*)$}, 1]
         name = CGI.unescape(name) if name.present?
         [name, locale]
