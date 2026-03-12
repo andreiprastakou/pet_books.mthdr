6aa28d08a06f4801cc6d301e2d7d8c39592c6ac3
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2026-03-12 09:12:59 +0100

    feat: rsuite bumped 4->6; years slider fixed (#85)
    * fix: rsuite 4->6 & slider fixed
    
    * fix: lint
    
    * chore: minimal css import

--- engines/frontend/app/javascript/widgets/booksListYearly/components/YearsSlider.jsx
+++ engines/frontend/app/javascript/widgets/booksListYearly/components/YearsSlider.jsx
@@ -1,17 +1,21 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Slider } from 'rsuite'
-import { useSelector } from 'react-redux'
+import 'rsuite/Slider/styles/index.css'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { jumpToYear } from 'widgets/booksListYearly/actions'
 import { selectCurrentYear, selectYears } from 'widgets/booksListYearly/selectors'
 
 const YearsSlider = () => {
   const [state, setState] = useState({ value: 0 })
+  const dispatch = useDispatch()
   const years = useSelector(selectYears())
   const currentYear = useSelector(selectCurrentYear())
 
   useEffect(() => currentYear && setState({ value: years.indexOf(currentYear) }), [currentYear])
 
   const handleChange = useCallback(v => setState({ value: v }), [])
+  const handleChangeCommitted = useCallback(i => dispatch(jumpToYear(years[i])), [dispatch, years])
 
   return (
     <div className='years-slider'>
@@ -21,10 +25,10 @@ const YearsSlider = () => {
         max={years.length - 1}
         min={0}
         onChange={handleChange}
+        onChangeCommitted={handleChangeCommitted}
         tooltip={false}
         value={state.value}
         vertical
-        // onChangeCommitted={ (i) => dispatch(jumpToYear(years[i])) }
       />
     </div>
   )
