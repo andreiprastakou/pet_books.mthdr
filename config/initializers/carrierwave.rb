0e146596e8724e3431b2945767b76bc46a1d9784
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2026-03-13 21:02:42 +0100

    fix: made aws init optional

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
 require 'fog/aws'
 
-CarrierWave.configure do |config|
-  fog_credentials = {
-    provider: 'AWS',
-    region: 'us-east-2',
-    aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-  }
-  Fog::Storage.new(fog_credentials).sync_clock
+if ENV['AWS_ACCESS_KEY_ID'] do
+  CarrierWave.configure do |config|
+    fog_credentials = {
+      provider: 'AWS',
+      region: 'us-east-2',
+      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    }
+    Fog::Storage.new(fog_credentials).sync_clock
 
-  config.fog_credentials = fog_credentials
-  config.fog_directory = 'infospace-books-app'
-  config.fog_public = false
+    config.fog_credentials = fog_credentials
+    config.fog_directory = 'infospace-books-app'
+    config.fog_public = false
+  end
 end
