f5f2d83d9769019ba70348238a711ed645583d69
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2026-08-30 23:10:42 +0200

    feat: tags redesigned

--- engines/frontend/app/javascript/components/TagBadge.jsx
+++ engines/frontend/app/javascript/components/TagBadge.jsx
@@ -1,28 +1,25 @@
 import React, { useCallback, useContext } from 'react'
 import { useSelector } from 'react-redux'
-import { Badge } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 import { selectTagRef, selectCategory } from 'store/tags/selectors'
 import UrlStoreContext from 'store/urlStore/Context'
 
-const TagBadge = ({ text, id = null, renderPostfix = null, classes = '', variant = 'light', onClick = null }) => {
+const TagBadge = ({ text, id = null, renderPostfix = null, classes = '', onClick = null }) => {
   const label = `#${text}`
   const tagRef = useSelector(selectTagRef(id))
   const category = useSelector(selectCategory(tagRef?.categoryId))
   const { routes: { tagPagePath }, actions: { goto }, routesReady } = useContext(UrlStoreContext)
-  const classnames = classNames(['tag-container', `tag-category-${category.name}`, classes])
+  const classnames = classNames(['tag-container', 'tag-badge', `tag-category-${category.name}`, classes])
   const clickHandler = useCallback(() => onClick ? onClick() : goto(tagPagePath(id)), [goto, tagPagePath])
 
   if (!tagRef || !category) return null
   if (!routesReady) return null
 
   return (
-    <Badge
+    <span
       className={classnames}
-      pill
-      variant={variant}
     >
       <a
         className='tag-name'
@@ -33,7 +30,7 @@ const TagBadge = ({ text, id = null, renderPostfix = null, classes = '', variant
       </a>
 
       { renderPostfix ? renderPostfix() : null }
-    </Badge>
+    </span>
   )
 }
 
@@ -46,7 +43,6 @@ TagBadge.propTypes = {
     PropTypes.string,
     PropTypes.number
   ]).isRequired,
-  variant: PropTypes.string,
 }
 
 export default TagBadge
