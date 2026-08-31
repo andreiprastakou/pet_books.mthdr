d0dcd49cf538d1e94c5757f5dbde86f48a2f303b
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2026-08-31 14:59:53 +0200

    fix: fixes for BooksYear

--- engines/frontend/app/javascript/pages/booksPage/PageConfigurer.jsx
+++ engines/frontend/app/javascript/pages/booksPage/PageConfigurer.jsx
@@ -1,7 +1,8 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { fetchCoverDesigns } from 'store/coverDesigns/actions'
+import { setCurrentBookId } from 'store/axis/actions'
 import { prepareNavRefs } from 'widgets/navbar/actions'
 import {
   addYears,
@@ -24,32 +25,38 @@ const BOOKS_PER_PAGE = 1000
 
 const PageConfigurer = () => {
   const dispatch = useDispatch()
-  const { routesReady } = useContext(UrlStoreContext)
+  const { pageState, routesReady } = useContext(UrlStoreContext)
+  const { bookId } = pageState
+  const bookIdStateReady = Object.hasOwn(pageState, 'bookId')
+  const hasConfigured = useRef(false)
 
   useEffect(() => {
-    if (!routesReady) return
+    if (hasConfigured.current || !routesReady || !bookIdStateReady) return
+    hasConfigured.current = true
 
     dispatch(setPageIsLoading(true))
     dispatch(clearListState())
     dispatch(clearYears())
     dispatch(assignPerPage(BOOKS_PER_PAGE))
     dispatch(assignPage(1))
-    dispatch(assignSortBy('name'))
+    dispatch(assignSortBy('random'))
 
     Promise.all([
       dispatch(prepareNavRefs()),
       dispatch(fetchCoverDesigns()),
       apiClient.getBooksYears(),
-    ]).then(([, , years]) => {
-      const year = years[years.length - 1]
+      bookId ? apiClient.getBookRefEntry(bookId) : Promise.resolve(null),
+    ]).then(([, , years, book]) => {
+      const year = book?.year || years[years.length - 1]
       dispatch(addYears(years))
       dispatch(assignFilter({ years: [year] }))
+      if (book) dispatch(setCurrentBookId(book.id))
       return dispatch(fetchBooks())
     }).then(() => {
       dispatch(switchToFirstBook())
       dispatch(setPageIsLoading(false))
     })
-  }, [routesReady])
+  }, [bookId, bookIdStateReady, routesReady])
 
   return null
 }
