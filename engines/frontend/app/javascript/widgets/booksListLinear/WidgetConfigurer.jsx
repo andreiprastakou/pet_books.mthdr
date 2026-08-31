18058e2cd04875ddedb8836542552d5ff63bb784
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2026-08-31 12:01:22 +0200

    feat: Tag page redesign

--- engines/frontend/app/javascript/widgets/booksListLinear/WidgetConfigurer.jsx
+++ engines/frontend/app/javascript/widgets/booksListLinear/WidgetConfigurer.jsx
@@ -1,11 +1,12 @@
 import { useContext, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import { selectPage, selectPerPage, selectSortBy } from 'widgets/booksListLinear/selectors'
 import { fetchBooks, switchToFirstBook } from 'widgets/booksListLinear/actions'
 import UrlStoreContext from 'store/urlStore/Context'
 
-const WidgetConfigurer = () => {
+const WidgetConfigurer = ({ selectFirstBook = true }) => {
   const dispatch = useDispatch()
   const page = useSelector(selectPage())
   const perPage = useSelector(selectPerPage())
@@ -15,11 +16,15 @@ const WidgetConfigurer = () => {
   if (!routesReady) return null
 
   useEffect(() => {
-    dispatch(fetchBooks()).then(() =>
-      dispatch(switchToFirstBook())
-    )
-  }, [page, perPage, sortBy])
+    dispatch(fetchBooks()).then(() => {
+      if (selectFirstBook) dispatch(switchToFirstBook())
+    })
+  }, [page, perPage, selectFirstBook, sortBy])
   return null
+}
+
+WidgetConfigurer.propTypes = {
+  selectFirstBook: PropTypes.bool,
 }
 
 export default WidgetConfigurer
